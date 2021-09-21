7269042f270913f79d6d40426ba15f114e44886f
randomthinkr <buboyn@magenic.com>
2021-09-21 16:18:09 +0800

    added logger and proper exception handling to serializer and deserializer classes

--- producer/src/main/java/dev/csv/buboyn/kafka/serializer/OrderSerializer.java
+++ producer/src/main/java/dev/csv/buboyn/kafka/serializer/OrderSerializer.java
@@ -3,17 +3,25 @@ package dev.csv.buboyn.kafka.serializer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.csv.buboyn.kafka.domain.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
 
+@Slf4j
 public class OrderSerializer implements Serializer<Order> {
     @Override
     public byte[] serialize(String topic, Order order) {
+
         try {
+            if (null == order){
+               log.info("Null received for serializing");
+                return null;
+            }
+            log.info("Serializing...");
             return new ObjectMapper().writeValueAsBytes(order);
+        } catch (JsonProcessingException e) {
+            throw new SerializationException("Error when serializing Order to byte[]");
         }
-        catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return null;
+
     }
 }
